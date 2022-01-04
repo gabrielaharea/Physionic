f209300cb243c802361fe77e0bc1410afeb210e4
Gabriela Harea <hareagabi@gmail.com>
2022-01-04 18:33:33 +0200

    add files

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,8 +1,35 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect, FC } from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import createEmotionCache from "./createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import { EmotionCache } from "@emotion/cache";
+import { theme } from "./theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  // eslint-disable-next-line react/require-default-props
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const App: FC<MyAppProps> = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default App;

84db7b63370cc1e6183d2aa77703d14b43c5a025
Gabriela Harea <hareagabi@gmail.com>
2022-01-13 19:11:15 +0200

    add box and grid

--- my-app/pages/components/ReviewCards/index.tsx
+++ my-app/pages/components/ReviewCards/index.tsx
@@ -7,6 +7,7 @@ import Paper from "@mui/material/Paper";
 import { FC, useState } from "react";
 import * as React from "react";
 import BasicRating from "../Rating";
+import Stack from "@mui/material/Stack";
 
 export const ReviewCards: React.FC<Props> = ({
   width,
@@ -19,11 +20,23 @@ export const ReviewCards: React.FC<Props> = ({
   return (
     <>
       <Card>
-        <Box>
-          <Paper>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: 'wrap',
+            "& > :not(style)": {
+              m: 2,
+              width: 330,
+              height: 244,
+            },
+          }}
+        >
+          <Paper elevation={0}>
             <BasicRating />
             <Typography>{rewiew}</Typography>
-            <Avatar alt="foto-people" src="/img/#user.1.jpg" />
+            <Stack direction="row" spacing={2}>
+              <Avatar alt="foto-people" src="./img/#user.1.jpg" />
+            </Stack>
           </Paper>
         </Box>
       </Card>
--- my-app/pages/components/ReviewCom/index.tsx
+++ my-app/pages/components/ReviewCom/index.tsx
@@ -1,16 +1,50 @@
 import Paragraf from "../Paragraf";
 import { ReviewCards } from "../ReviewCards";
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
-const RewiewPages = () => {
+const RewiewPages = [
+  {
+    rewiew:
+      "Slate helps you see how many more days you need to work to reach your financial goal.",
+  },
+  {
+    rewiew:
+      "Slate helps you see how many more days you need to work to reach your financial goal.",
+  },
+  {
+    rewiew:
+      "Slate helps you see how many more days you need to work to reach your financial goal.",
+  },
+];
+
+export default function CardsRewiewPages() {
   return (
-    <div>
+    <Box p={9}>
       <Paragraf />
-      <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
-      <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
-      <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
-    </div>
+      <Grid container spacing={5}>
+        {RewiewPages.map((RewiewPages, i) => {
+          return (
+            <Grid key={i} item>
+              <ReviewCards {...RewiewPages} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
-};
+}
 
-export default RewiewPages;
+// const RewiewPages = () => {
+//   return (
+//     <div>
+//       <Paragraf />
+//       <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
+//       <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
+//       <ReviewCards rewiew="Slate helps you see how many more days you need to work to reach your financial goal." />
+//     </div>
+//   );
+// };
+
+// export default RewiewPages;
